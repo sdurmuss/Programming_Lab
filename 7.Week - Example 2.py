462ff452fc1d7e6342897af02c9a74ea0b5c700b
sdurmuss <48418784+sdurmuss@users.noreply.github.com>
2019-04-09 21:48:26 +0300

    olasılık hesaplamaları

---
+++ 7.Week - Example 2.py
@@ -0,0 +1,67 @@
+import random
+
+def rollDie():
+    """Returns a random int between 1 and 6"""
+    return random.choice([1,2,3,4,5,6])
+
+def rollN(n):
+    result = ''
+    for i in range(n):
+        result = result + str(rollDie()) + " "
+    print(result)
+    
+rollN(10)
+print(rollDie())
+
+
+def flip(numFlips):
+    """Assumes numFlips a positive int"""
+    heads = 0
+    for i in range(numFlips):
+        if random.choice(('H', 'T')) == 'H':
+            heads += 1
+    return heads/numFlips
+    
+print(flip(5000))    
+
+    
+def flipSim(numFlipsPerTrial, numTrials):
+    """Assumes numFlipsPerTrial and numTrials positive ints"""
+    fracHeads = []
+    for i in range(numTrials):
+        fracHeads.append(flip(numFlipsPerTrial))
+    mean = sum(fracHeads)/len(fracHeads)
+    return mean
+    
+print(flipSim(10,3))
+
+
+import pylab
+def flipPlot(minExp, maxExp):
+    """Assumes minExp and maxExp positive integers; minExp < maxExp
+    Plots results of 2**minExp to 2**maxExp coin flips"""
+    ratios, diffs, xAxis = [], [], []
+    for exp in range(minExp, maxExp + 1):
+        xAxis.append(2**exp)
+    for numFlips in xAxis:
+        numHeads = 0
+        for n in range(numFlips):
+            if random.choice(('H', 'T')) == 'H':
+                numHeads += 1
+        numTails = numFlips - numHeads
+        try:
+            ratios.append(numHeads/numTails)
+            diffs.append(abs(numHeads - numTails))
+        except ZeroDivisionError:
+            continue
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails)')
+    pylab.plot(xAxis, diffs, 'k')
+    pylab.figure()
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('#Heads/#Tails')
+    pylab.plot(xAxis, ratios, 'k')
+random.seed(0)
+flipPlot(4, 20)
